98b9ed870d55a05ef88459c9a381c9fc1f1df0ca
bauticalvo <bito.bc@gmail.com>
2025-05-21 19:15:11 -0300

    add infosection

---
+++ src/Components/Home/InfoSection.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const InfoSection = () => {
+
+    return(
+        <div className="min-h-screen w-full flex">
+            <h1>Infosection</h1>
+        </div>
+    )
+
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import { FirstProduct } from "../Components/Home/FirstProduct";
 import { HeroSection } from "../Components/Home/HeroSection";
+import { InfoSection } from "../Components/Home/InfoSection";
 import { SecondProduct } from "../Components/Home/SecondProduct";
 
 
@@ -8,6 +9,7 @@ export const Home = () => {
   return (
     <div className="min-h-[90vh] mt-[7.1vh] bg-background flex flex-col justify-center items-center">
         <HeroSection />
+        <InfoSection />
         <FirstProduct />
         <SecondProduct />
     </div>
